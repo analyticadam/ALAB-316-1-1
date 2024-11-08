34b73ca7107b40e82caecbddab0ca1803968b196
Adam Farley <farleyadamc@gmail.com>
2024-11-07 21:52:05 -0600

    updated comments in code, final commit and assignment completed

--- index.js
+++ index.js
@@ -76,67 +76,74 @@ function aHandler(event) {
   } else {
     event.preventDefault()
     console.log(event.target.textContent);  // Log the content of the <a> to verify the handler is working.
-  }}
-    // All events load in refresh
-    document.addEventListener('DOMContentLoaded', () => {
- 
-      for (let i =0; i < topMenuLinks.length; i++){
-        const clicked = topMenuLinks[i]
-         console.log(clicked);
-    
-        clicked.addEventListener("click", function(e){
-            if(!e.target.matches('a'))return
-            //e.target targets a tag
-            console.log(e.target);
-            e.target.classList.toggle('active') //Toggle
-            topMenuLinks.forEach(link=>{
-              console.log(link.subLinks);
-              if(link!==e.target){
-                 link.classList.remove('active')
-              }
-              const clickedLink = menuLinks.find((link)=>link.text==e.target.textContent)
-              console.log(clickedLink.subLinks);
-              if(e.target.classList.contains('active')&& e.target.innerHTML!== "about"){
-                subMenuEl.style.top="100%"
-                buildSubMenu(clickedLink.subLinks)
-              }else{
-                subMenuEl.style.top="0%"
-              }
-            })
-              function buildSubMenu(subLinks){
-                console.log(subLinks);
-                // const subMenuEl = document.querySelectorAll("sub-menu")
-                console.log(subMenuEl);
-                subMenuEl.innerHTML =""
-                subLinks.forEach(link=>{
-                  const a = document.createElement('a')
-                  a.setAttribute('href', link.href)
-                  a.textContent= link.text
-                  subMenuEl.appendChild(a)
-                })
-              }
-              subMenuEl.addEventListener("click", function(e){
-                e.preventDefault()
-                if(!e.target.matches('a')){
-                  return;
-                } 
-                console.log(e.target);
-                subMenuEl.style.top ="0%"
-                topMenuLinks.forEach(link=>{
-                  link.classList.remove('active')
-                })
-                h1.textContent =`${e.target.textContent}`
-              })
+  }
+}
+// All events load in refresh
+document.addEventListener('DOMContentLoaded', () => {
+
+
+  for (let i = 0; i < topMenuLinks.length; i++) {
+    const clicked = topMenuLinks[i]
+    console.log(clicked);
+
+    clicked.addEventListener("click", function (e) {
+      if (!e.target.matches('a')) return
+      //e.target targets a tag
+      console.log(e.target);
+      // 1.  The event listener should add the active class to the <a> element that was clicked, unless it was
+      // already active, in which case it should remove it.
+      e.target.classList.toggle('active') //Toggle
+      topMenuLinks.forEach(link => {
+        console.log(link.subLinks);
+        if (link !== e.target) {
+          link.classList.remove('active') // 2.  The event listener should remove the active class from each other <a> element
+          // in topMenuLinks - whether the active class exists or not.
+        }
+        //1. Within the event listener, if the clicked <a> element does not yet have a class of "active" (it was inactive when clicked):
+        // a. If the clicked <a> element's "link" object within menuLinks has a subLinks property (all do, except for the "link" object for
+        // ABOUT), set the CSS top property of subMenuEl to 100%.
+        // b.Otherwise, set the CSS top property of subMenuEl to 0.
+        const clickedLink = menuLinks.find((link) => link.text == e.target.textContent)
+        console.log(clickedLink.subLinks);
+        if (e.target.classList.contains('active') && e.target.innerHTML !== "about") {
+          subMenuEl.style.top = "100%"
+          buildSubMenu(clickedLink.subLinks)
+        } else {
+          subMenuEl.style.top = "0%"
+        }
       })
-    }
-      
-    });
 
-    
+      function buildSubMenu(subLinks) {
+        console.log(subLinks);
+        // const subMenuEl = document.querySelectorAll("sub-menu")
+        console.log(subMenuEl);
+        // 1. Clear the current contents of subMenuEl. 
+        subMenuEl.innerHTML = ""
+        // 2. Iterate over the subLinks array, passed as an argument, and for each "link" object: 
+        subLinks.forEach(link => {
+          // a. Create an <a> element. 
+          const a = document.createElement('a')
+          // b. Add an href attribute to the <a>, with the value set by the href property of the "link" object. 
+          a.setAttribute('href', link.href)
+          // c. Set the element's content to the value of the text property of the "link" object. 
+          a.textContent = link.text
+          // d. Append the new element to the subMenuEl.
+          subMenuEl.appendChild(a)
+        })
+      }
+      subMenuEl.addEventListener("click", function (e) {
+        e.preventDefault()
+        if (!e.target.matches('a')) {
+          return;
+        }
+        console.log(e.target);
+        subMenuEl.style.top = "0%"
+        topMenuLinks.forEach(link => {
+          link.classList.remove('active')
+        })
+        h1.textContent = `${e.target.textContent}`
+      })
+    })
+  }
 
-
-// 1.  The event listener should add the active class to the <a> element that was clicked, unless it was
-// already active, in which case it should remove it.
-// 2.  The event listener should remove the active class from each other <a> element
-// in topMenuLinks - whether the active class exists or not.
-// Hint: Removing a non-existent class from an element does not cause an error!
+});
